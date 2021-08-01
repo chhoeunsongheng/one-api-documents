7edcfc15434710cffcfbbc3e7517e2fa890cd187
Chhouen Songheng <chhoeun.songheng@sabay.com>
2021-08-01 17:06:27 +0700

    initial api docs project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { RedocStandalone } from 'redoc';
@@ -8,10 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <RedocStandalone specUrl="https://redocly.github.io/redoc/openapi.yaml" />
         <a
           className="App-link"
